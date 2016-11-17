e469c989ac2dba3064c1497951fb900a0e4c9cc3
testertesting123 <jontest22@yahoo.com>
2016-11-17 13:32:10 -0500

    Added eraser button, pencil button, added stroke to points, gui restructured
    gui looks halfway decent now, some buttons have been implemented, sound
    + recording has not been started yet.

--- DrawClient/src/drawclient/DrawClient.java
+++ DrawClient/src/drawclient/DrawClient.java
@@ -5,9 +5,12 @@
  */
 package drawclient;
 
+import java.awt.BasicStroke;
 import java.awt.Color;
+import java.awt.Cursor;
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Image;
@@ -29,6 +32,7 @@ import javax.swing.JColorChooser;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+import javax.swing.SwingConstants;
 import javax.swing.colorchooser.AbstractColorChooserPanel;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
@@ -44,29 +48,39 @@ public class DrawClient extends JPanel
     {
         private int xpos, ypos, time, pgNum;
         private Color color;
+        private BasicStroke stroke;
+        
         public int getXPos() { return xpos;}
         public int getYPos() { return ypos;}
         public int getTime() { return time;}
         public int getPage() { return pgNum;}
         public Color getColor() { return color;}
-        Point(int x, int y, int t, Color color, int page)
+        public BasicStroke getStroke() {return stroke;}
+        Point(int x, int y, int t, Color color, int page, BasicStroke stroke)
         {
             xpos = x;
             ypos = y;
             time = t;
             this.color = color;
             this.pgNum = page;
+            this.stroke = stroke;
         }
     }
     private final ArrayList<ArrayList<ArrayList<Point>>> directory = new ArrayList<>();
     private final JButton undoButton;
-    private JButton prevButton;
-    private JButton nextButton;
+    private final JButton prevButton;
+    private final JButton nextButton;
+    private final JButton recordButton;
+    private final JButton eraserButton;
+    private final JButton drawButton;
     private final JColorChooser colorChooser;
     private Color color;
     private int page;
     private final JScrollPane scrollPanel;
-    private final JPanel panel;
+    private final JPanel mainPanel = new JPanel();
+    private BasicStroke strokeSize = new BasicStroke(1);
+    private BasicStroke lastStroke = strokeSize;
+    private Color lastColor = Color.BLACK;
     DrawClient()
     {
         page = 0;
@@ -96,8 +110,15 @@ public class DrawClient extends JPanel
         this.addMouseListener(this);
         prevButton = new JButton("Prev Page");
         nextButton = new JButton("Next Page");
+        recordButton = new JButton("Record");
+        eraserButton = new JButton("Eraser");
+        drawButton = new JButton("Draw");
+        
         undoButton.setEnabled(false);
         prevButton.setEnabled(false);
+        recordButton.setEnabled(true);
+        eraserButton.setEnabled(true);
+        drawButton.setEnabled(false);
         
         //allows you to change pages(previous page)
         prevButton.addActionListener(new ActionListener()
@@ -164,32 +185,84 @@ public class DrawClient extends JPanel
                
         });
         
+        eraserButton.addActionListener(new ActionListener()
+        {
+            @Override
+            public void actionPerformed(ActionEvent e) 
+            {
+                lastColor = color;
+                lastStroke = strokeSize;
+                color = Color.WHITE;
+                strokeSize = new BasicStroke(10);
+                eraserButton.setEnabled(false);
+                drawButton.setEnabled(true);
+            }
+               
+        });
+                
+        drawButton.addActionListener(new ActionListener()
+        {
+            @Override
+            public void actionPerformed(ActionEvent e) 
+            {
+                color = lastColor;
+                strokeSize = lastStroke;
+                eraserButton.setEnabled(true);
+                drawButton.setEnabled(false);
+            }
+               
+        });
+        
         Image img;
         try {
             img = ImageIO.read(getClass().getResource("/resources/UndoButton.png"));
             img = img.getScaledInstance( 50, 50,  java.awt.Image.SCALE_SMOOTH ) ;
             undoButton.setIcon(new ImageIcon(img));
-            undoButton.setBorderPainted(false);
-            undoButton.setBorder(null);
+            undoButton.setRolloverEnabled(true);
             undoButton.setMargin(new Insets(0, 0, 0, 0));
             undoButton.setContentAreaFilled(false);
+            undoButton.setVerticalTextPosition(SwingConstants.BOTTOM);
+            undoButton.setHorizontalTextPosition(SwingConstants.CENTER);
             
             img = ImageIO.read(getClass().getResource("/resources/BackButton.png"));
             img = img.getScaledInstance( 50, 50,  java.awt.Image.SCALE_SMOOTH ) ;
             prevButton.setIcon(new ImageIcon(img));
-            prevButton.setBorderPainted(false);
-            prevButton.setBorder(null);
             prevButton.setMargin(new Insets(0, 0, 0, 0));
             prevButton.setContentAreaFilled(false);
+            prevButton.setVerticalTextPosition(SwingConstants.BOTTOM);
+            prevButton.setHorizontalTextPosition(SwingConstants.CENTER);
             
             img = ImageIO.read(getClass().getResource("/resources/ForwardButton.png"));
             img = img.getScaledInstance( 50, 50,  java.awt.Image.SCALE_SMOOTH ) ;
             nextButton.setIcon(new ImageIcon(img));
-            nextButton.setBorderPainted(false);
-            nextButton.setBorder(null);
             nextButton.setMargin(new Insets(0, 0, 0, 0));
             nextButton.setContentAreaFilled(false);
+            nextButton.setVerticalTextPosition(SwingConstants.BOTTOM);
+            nextButton.setHorizontalTextPosition(SwingConstants.CENTER);
             
+            img = ImageIO.read(getClass().getResource("/resources/RecordButton.png"));
+            img = img.getScaledInstance( 50, 50,  java.awt.Image.SCALE_SMOOTH ) ;
+            recordButton.setIcon(new ImageIcon(img));
+            recordButton.setMargin(new Insets(0, 0, 0, 0));
+            recordButton.setContentAreaFilled(false);
+            recordButton.setVerticalTextPosition(SwingConstants.BOTTOM);
+            recordButton.setHorizontalTextPosition(SwingConstants.CENTER);
+            
+            img = ImageIO.read(getClass().getResource("/resources/Eraser.png"));
+            img = img.getScaledInstance( 50, 50,  java.awt.Image.SCALE_SMOOTH ) ;
+            eraserButton.setIcon(new ImageIcon(img));
+            eraserButton.setMargin(new Insets(0, 0, 0, 0));
+            eraserButton.setContentAreaFilled(false);
+            eraserButton.setVerticalTextPosition(SwingConstants.BOTTOM);
+            eraserButton.setHorizontalTextPosition(SwingConstants.CENTER);
+            
+            img = ImageIO.read(getClass().getResource("/resources/pencil.png"));
+            img = img.getScaledInstance( 50, 50,  java.awt.Image.SCALE_SMOOTH ) ;
+            drawButton.setIcon(new ImageIcon(img));
+            drawButton.setMargin(new Insets(0, 0, 0, 0));
+            drawButton.setContentAreaFilled(false);
+            drawButton.setVerticalTextPosition(SwingConstants.BOTTOM);
+            drawButton.setHorizontalTextPosition(SwingConstants.CENTER);
             
         } catch (IOException ex) {
             Logger.getLogger(DrawClient.class.getName()).log(Level.SEVERE, null, ex);
@@ -198,7 +271,7 @@ public class DrawClient extends JPanel
         
         
         
-        
+        JPanel panel = new JPanel();
         
         setPreferredSize(Toolkit.getDefaultToolkit().getScreenSize());
         scrollPanel = new JScrollPane(this);
@@ -220,9 +293,9 @@ public class DrawClient extends JPanel
         c.gridy = 0;
         panel.add(nextButton,c);
         
-//      c.gridx = 3;
-//      c.gridy = 0;
-//      panel.add(recordButton,c);
+        c.gridx = 3;
+        c.gridy = 0;
+        panel.add(recordButton,c);
         
         c.gridx = 4;
         c.gridy = 0;
@@ -230,23 +303,36 @@ public class DrawClient extends JPanel
         c.gridheight = 1;
         panel.add(colorChooser,c);
         
-//        c.gridx = 0;
-//        c.gridy = 0;
-//        c.gridwidth = 1;
-//        c.gridheight = 1;
-//        panel.add(eraserButton,c);
+        c.gridx = 7;
+        c.gridy = 0;
+        c.gridwidth = 1;
+        c.gridheight = 1;
+        panel.add(eraserButton,c);
+        
+        c.gridx = 8;
+        c.gridy = 0;
+        c.gridwidth = 1;
+        c.gridheight = 1;
+        panel.add(drawButton,c);
         
         c.fill = GridBagConstraints.BOTH;
         c.gridx = 0;
         c.gridy = 1;
-        c.weighty = 1;
-        c.weightx = 1;
-        panel.add(scrollPanel,c);
+        c.weighty = .2;
+        mainPanel.add(panel,c);
+        
+        
+        c.fill = GridBagConstraints.BOTH;
+        c.gridx = 0;
+        c.gridy = 1;
+        c.weighty = .8;
+        mainPanel.add(scrollPanel,c);
+        
     }
     
     public JPanel getPanel()
     {
-        return panel;
+        return mainPanel;
     }
     
     //for detecting color changes
@@ -277,7 +363,8 @@ public class DrawClient extends JPanel
     @Override
     public void paintComponent(Graphics g)
     {
-        super.paintComponent(g);
+        Graphics2D g2 = (Graphics2D) g;
+        super.paintComponent(g2);
         synchronized(directory)
         {
             int l = 0;
@@ -285,16 +372,21 @@ public class DrawClient extends JPanel
             {
                 for(Point point : ary)
                 {
-                    g.setColor(point.getColor());
+                    g2.setPaint(point.getColor());
+                    g2.setStroke(point.getStroke());
                     int index = directory.get(page).get(l).indexOf(point);
                     if(index != 0)
-                        g.drawLine(directory.get(page).get(l).get(index-1).getXPos(),
+                    {
+                        g2.drawLine(directory.get(page).get(l).get(index-1).getXPos(),
                                    directory.get(page).get(l).get(index-1).getYPos(), 
                                    point.getXPos(), 
                                    point.getYPos());
+                    }
                     //if you press the mouse but dont drag the mouse, still draw a dot
                     else 
-                        g.fillRect(point.getXPos(), point.getYPos(), 1,1);
+                    {
+                        g2.fillRect(point.getXPos(), point.getYPos(), 1,1);
+                    }
                     
                 }
                 l++;
@@ -330,7 +422,7 @@ public class DrawClient extends JPanel
     @Override
     public void mouseDragged(MouseEvent e) 
     {
-        Point point = new Point(e.getX(),e.getY(),0, color, page);
+        Point point = new Point(e.getX(),e.getY(),0, color, page,strokeSize);
         directory.get(page).get(directory.get(page).size()-1).add(point);
         repaint();    
         
@@ -350,7 +442,7 @@ public class DrawClient extends JPanel
         undoButton.setEnabled(true);
         directory.get(page).add(new ArrayList<>());
         //if you press the mouse make a starting point
-        Point point = new Point(e.getX(),e.getY(),0, color, page);
+        Point point = new Point(e.getX(),e.getY(),0, color, page,strokeSize);
         directory.get(page).get(directory.get(page).size()-1).add(point);
         
     }
@@ -361,10 +453,12 @@ public class DrawClient extends JPanel
 
     @Override
     public void mouseEntered(MouseEvent e) {
+        setCursor(Cursor.getPredefinedCursor(Cursor.CROSSHAIR_CURSOR));
     }
 
     @Override
     public void mouseExited(MouseEvent e) {
+        setCursor(Cursor.getDefaultCursor());
     }
     
 }
